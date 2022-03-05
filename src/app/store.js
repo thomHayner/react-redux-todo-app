1a2da15329bf63ebe5742b34f85c168771fae4e1
Thomas Hayner <thom.hayner@gmail.com>
2022-03-05 02:26:58 -0700

    code: update store for tasksReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import tasksReducer from '../components/toDoList/tasksSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    todoList: tasksReducer,
   },
 });
