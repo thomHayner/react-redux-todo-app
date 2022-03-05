66ae833c41d6f1a878e88a123a133916d9508074
Thomas Hayner <thom.hayner@gmail.com>
2022-03-05 03:41:32 -0700

    code: update reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from '../components/toDoList/tasksSlice';
+import todoReducer from '../components/toDoList/tasksSlice';
 
 export const store = configureStore({
   reducer: {
-    todoList: tasksReducer,
+    todoList: todoReducer,
   },
 });
