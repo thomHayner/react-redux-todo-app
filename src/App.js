a04e438cf0c9bd5292ff83185c584fc8c776d805
Thomas Hayner <thom.hayner@gmail.com>
2022-03-06 16:40:53 -0700

    code: remove boilerplate, add custom components

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import AddNewTaskCard from './components/toDoList/elements/AddNewTaskCard';
+import ToDoList from './components/toDoList/ToDoList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div 
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#0A104F",
+      }}
+    >
+      <div
+        className="content-wrapper"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          minWidth: "350px",
+          maxWidth: "33vw",
+          backgroundColor: "#653BCD",
+        }}
+      >
+        <h4>Todo App</h4>
+        <AddNewTaskCard />
+        <ToDoList />
+      </div>
     </div>
   );
 }
