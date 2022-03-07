953f81ed3411841c5fc222f8233cc4445874b5f0
Thomas Hayner <thom.hayner@gmail.com>
2022-03-06 21:02:40 -0700

    code: convert to styled components

--- src/components/toDoList/ToDoList.jsx
+++ src/components/toDoList/ToDoList.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-import TodoActiveItemsList from './elements/ActiveItemsList';
-import TodoCompletedItemList from './elements/CompletedTasksList.jsx';
+import TodoActiveItemsList from "./elements/ActiveItemsList";
+import TodoCompletedItemList from "./elements/CompletedTasksList.jsx";
+
+const ComponentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin-left: -20px;
+  padding: 0px;
+  row-gap: 20px;
+`;
+
+const DashedAreaDivider = styled.div`
+  width: 100%;
+  border-bottom: 2px dashed lightgrey;
+  margin-left: 10px;
+`;
 
 const ToDoList = () => {
   return(
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "80%",
-        marginLeft: "-20px",
-        padding: "0px",
-        rowGap: "20px",
-      }}
-    >
+    <ComponentWrapper>
       <TodoActiveItemsList />
-      <div
-        style={{
-          width: "100%",
-          borderBottom: "2px dashed lightgrey",
-          marginLeft: "10px",
-        }}
-      />
+      <DashedAreaDivider />
       <TodoCompletedItemList />
-    </div>
+    </ComponentWrapper>
   )
 };
 
